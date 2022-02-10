61ef5a877670e99bd32421b76de2bc99add25037
JosiahDadulo <moleculej22@gmail.com>
2022-02-10 10:55:09 -0600

    Changes: I updated App.js with comments that seperate the different components of the UI.

--- App.js
+++ App.js
@@ -5,6 +5,12 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Start of Nada! Let's get this going!</Text>
+      {/*Icon to go and edit users profile*/}
+      {/* Reel System*/}
+      {/*Profiles to match with*/}
+      {/*Reel System*/} 
+      {/*Task bar in lower app which includes button to go back to profiles and another
+      to see profiles that sent a response to users profile*/}
       <StatusBar style="auto" />
     </View>
   );
