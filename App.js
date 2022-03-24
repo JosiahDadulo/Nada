ca426ca51717946cf6edfab414864938d910ee9f
JosiahDadulo <moleculej22@gmail.com>
2022-03-24 13:46:20 -0500

    figured out potential reel system with flatlist and side scroll view

--- App.js
+++ App.js
@@ -7,15 +7,15 @@ import Activities from "./navigation/screens/Activities";
 import PhoneNumber from "./navigation/screens/PhoneNumber"
 
 const Stack = createStackNavigator();
-//<Stack.Screen name ="Phone Number" component={PhoneNumber}/>
-      //<Stack.Screen name ="SignUp" component={Signup} />
-      //<Stack.Screen name ="Activities" component={Activities}/>
+
 export default () =>{
   return(
     <NavigationContainer independent={true}>
       <Stack.Navigator>
-      
-      <Stack.Screen name ="Home" component={MainContainer} options={{headerShown: false, headerLeft: false }}/>
+        <Stack.Screen name ="Phone Number" component={PhoneNumber}/>
+        <Stack.Screen name ="SignUp" component={Signup} />
+        <Stack.Screen name ="Activities" component={Activities}/>
+        <Stack.Screen name ="Home" component={MainContainer} options={{headerShown: false, headerLeft: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
     )
